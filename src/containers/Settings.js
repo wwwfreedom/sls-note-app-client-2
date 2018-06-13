8587be2e1ecce38df3305cdbc84b386a15e69f7e
Kevin <tig3r86@gmail.com>
2018-06-13 23:15:23 +1000

    connecting the billing form

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react"
 import { API } from "aws-amplify"
+// The StripeProvider component let’s the Stripe SDK know that we want to call the Stripe methods using config.STRIPE_KEY. And it needs to wrap around at the top level of our billing form.
+// the Elements component needs to wrap around any component that is going to be using the CardElement Stripe component.
+import { Elements, StripeProvider } from "react-stripe-elements"
+import BillingForm from "../components/BillingForm"
+import config from "../config"
+import "./Settings.css"
 
 export default class Settings extends Component {
   constructor(props) {
@@ -16,7 +22,40 @@ export default class Settings extends Component {
     })
   }
 
+  handleFormSubmit = async (storage, { token, error }) => {
+    if (error) {
+      alert(error)
+      return
+    }
+
+    this.setState({ isLoading: true })
+
+    try {
+      await this.billUser({
+        storage,
+        source: token.id,
+      })
+
+      alert("Your card has been charged successfully!")
+      this.props.history.push("/")
+    } catch (e) {
+      alert(e)
+      this.setState({ isLoading: false })
+    }
+  }
+
   render() {
-    return <div className="Settings" />
+    return (
+      <div className="Settings">
+        <StripeProvider apiKey={config.STRIPE_KEY}>
+          <Elements>
+            <BillingForm
+              loading={this.state.isLoading}
+              onSubmit={this.handleFormSubmit}
+            />
+          </Elements>
+        </StripeProvider>
+      </div>
+    )
   }
 }
