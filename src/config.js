ed74b5c3d20db591d84c0ed13aa9935be4dfd105
Kevin <tig3r86@gmail.com>
2018-06-13 22:48:33 +1000

    adding stripe keys to config

--- src/config.js
+++ src/config.js
@@ -1,44 +1,44 @@
 const dev = {
   s3: {
     REGION: "ap-southeast-2",
-    BUCKET: "notes-app-2-api-dev-attachmentsbucket-1ud39z516qqtp"
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-1ud39z516qqtp",
   },
   apiGateway: {
     REGION: "ap-southeast-2",
-    URL: "https://avksfutcy4.execute-api.ap-southeast-2.amazonaws.com/dev"
+    URL: "https://avksfutcy4.execute-api.ap-southeast-2.amazonaws.com/dev",
   },
   cognito: {
     REGION: "ap-southeast-2",
     USER_POOL_ID: "ap-southeast-2_kdgfdFm1Z",
     APP_CLIENT_ID: "ibbjk6g31b4uu35v1ncfv0rd",
-    IDENTITY_POOL_ID: "ap-southeast-2:ce30a4a8-e04d-4281-ba40-632ad11dd87f"
-  }
-};
+    IDENTITY_POOL_ID: "ap-southeast-2:ce30a4a8-e04d-4281-ba40-632ad11dd87f",
+  },
+  STRIPE_KEY: "pk_test_mpREoFSqeLyd8uJrXWPoeUAy",
+}
 
 const prod = {
   s3: {
     REGION: "ap-southeast-2",
-    BUCKET: "notes-app-2-api-prod-attachmentsbucket-hws305ofajfu"
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-hws305ofajfu",
   },
   apiGateway: {
     REGION: "ap-southeast-2",
-    URL: "https://gsvoo5yid6.execute-api.ap-southeast-2.amazonaws.com/prod"
+    URL: "https://gsvoo5yid6.execute-api.ap-southeast-2.amazonaws.com/prod",
   },
   cognito: {
     REGION: "ap-southeast-2",
     USER_POOL_ID: "ap-southeast-2_Bqmj1cNJe",
     APP_CLIENT_ID: "2dmh7al69fg1c77ijm85s2c0d1",
-    IDENTITY_POOL_ID: "ap-southeast-2:4a900ed6-99db-443c-88f6-3742060d8eb2"
-  }
-};
+    IDENTITY_POOL_ID: "ap-southeast-2:4a900ed6-99db-443c-88f6-3742060d8eb2",
+  },
+  STRIPE_KEY: "pk_test_mpREoFSqeLyd8uJrXWPoeUAy",
+}
 
 // Default to dev if not set
-const config = process.env.REACT_APP_STAGE === 'prod'
-  ? prod
-  : dev;
+const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev
 
 export default {
   // Add common config values here
   MAX_ATTACHMENT_SIZE: 5000000,
-  ...config
-};
+  ...config,
+}
